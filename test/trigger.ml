d9aec0e22b7c2c0f50c8c61c6d896c1ef15f89bd
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-12-02 16:47:21 +0100

    Dom_html changes

--- test/trigger.ml
+++ test/trigger.ml
@@ -22,7 +22,7 @@ let%expect_test "empty div with triggered callback (failing)" =
       (Node.div
          ~attrs:
            [ Attr.on_click (fun e ->
-               print_s [%message (e##.screenX : int)];
+               print_s [%message (Js_of_ocaml.Js.to_float e##.screenX : float)];
                Effect.Ignore)
            ]
          [])
